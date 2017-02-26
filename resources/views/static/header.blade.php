e89ada22b356c5fd66735721125f67db8924c516
Rob Biemans <robbiemans500@gmail.com>
2017-02-26 23:13:55 +0100

    updated development

--- resources/views/static/header.blade.php
+++ resources/views/static/header.blade.php
@@ -38,6 +38,34 @@
               Contact
             </a>
           </li>
+          @if (Auth::guest())
+              <li><a href="{{ route('login') }}">Inloggen</a></li>
+              <li><a href="{{ route('register') }}">Registreren</a></li>
+          @else
+              <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                      {{ Auth::user()->name }} <span class="caret"></span>
+                  </a>
+
+                  <ul class="dropdown-menu" role="menu">
+                      <li>
+                          <a href="#">
+                              <i class="material-icons">shopping_cart</i>
+                              Winkelwagen
+                          </a>
+                          <a href="{{ route('logout') }}"
+                              onclick="event.preventDefault();
+                                       document.getElementById('logout-form').submit();">
+                              Uitloggen
+                          </a>
+
+                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                              {{ csrf_field() }}
+                          </form>
+                      </li>
+                  </ul>
+              </li>
+          @endif
        </ul>
       </div>
   </div>
