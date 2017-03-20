2f4041f456d07fece0a2487b76a9149469b69a6b
Rob Biemans <robbiemans500@gmail.com>
2017-03-20 10:04:25 +0100

    Fix migrate bug and added admin/admin@doggofood.nl

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+         $this->call(UserSeederTable::class);
          $this->call(ProductenSeederTable::class);
          $this->call(CategorySeederTable::class);
          $this->call(SubcategorySeederTable::class);
